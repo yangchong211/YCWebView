77899b4cd202d61dc45b38b850bea846e766d107
yangchong <yangchong211@163.com>
2019-11-14 15:50:09 +0800

    添加实现js交互的另一种思路代码

--- WebViewLib/src/main/java/com/ycbjie/webviewlib/Message.java
+++ WebViewLib/src/main/java/com/ycbjie/webviewlib/Message.java
@@ -123,16 +123,11 @@ public class Message {
         Message m =  new Message();
         try {
             JSONObject jsonObject = new JSONObject(jsonStr);
-            m.setHandlerName(jsonObject.has(HANDLER_NAME_STR)
-                    ? jsonObject.getString(HANDLER_NAME_STR):null);
-            m.setCallbackId(jsonObject.has(CALLBACK_ID_STR)
-                    ? jsonObject.getString(CALLBACK_ID_STR):null);
-            m.setResponseData(jsonObject.has(RESPONSE_DATA_STR)
-                    ? jsonObject.getString(RESPONSE_DATA_STR):null);
-            m.setResponseId(jsonObject.has(RESPONSE_ID_STR)
-                    ? jsonObject.getString(RESPONSE_ID_STR):null);
-            m.setData(jsonObject.has(DATA_STR)
-                    ? jsonObject.getString(DATA_STR):null);
+            m.setHandlerName(jsonObject.has(HANDLER_NAME_STR) ? jsonObject.getString(HANDLER_NAME_STR):null);
+            m.setCallbackId(jsonObject.has(CALLBACK_ID_STR) ? jsonObject.getString(CALLBACK_ID_STR):null);
+            m.setResponseData(jsonObject.has(RESPONSE_DATA_STR) ? jsonObject.getString(RESPONSE_DATA_STR):null);
+            m.setResponseId(jsonObject.has(RESPONSE_ID_STR) ? jsonObject.getString(RESPONSE_ID_STR):null);
+            m.setData(jsonObject.has(DATA_STR) ? jsonObject.getString(DATA_STR):null);
             return m;
         } catch (JSONException e) {
             e.printStackTrace();
@@ -147,16 +142,11 @@ public class Message {
             for(int i = 0; i < jsonArray.length(); i++){
                 Message m = new Message();
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                m.setHandlerName(jsonObject.has(HANDLER_NAME_STR)
-                        ? jsonObject.getString(HANDLER_NAME_STR):null);
-                m.setCallbackId(jsonObject.has(CALLBACK_ID_STR)
-                        ? jsonObject.getString(CALLBACK_ID_STR):null);
-                m.setResponseData(jsonObject.has(RESPONSE_DATA_STR)
-                        ? jsonObject.getString(RESPONSE_DATA_STR):null);
-                m.setResponseId(jsonObject.has(RESPONSE_ID_STR)
-                        ? jsonObject.getString(RESPONSE_ID_STR):null);
-                m.setData(jsonObject.has(DATA_STR)
-                        ? jsonObject.getString(DATA_STR):null);
+                m.setHandlerName(jsonObject.has(HANDLER_NAME_STR) ? jsonObject.getString(HANDLER_NAME_STR):null);
+                m.setCallbackId(jsonObject.has(CALLBACK_ID_STR) ? jsonObject.getString(CALLBACK_ID_STR):null);
+                m.setResponseData(jsonObject.has(RESPONSE_DATA_STR) ? jsonObject.getString(RESPONSE_DATA_STR):null);
+                m.setResponseId(jsonObject.has(RESPONSE_ID_STR) ? jsonObject.getString(RESPONSE_ID_STR):null);
+                m.setData(jsonObject.has(DATA_STR) ? jsonObject.getString(DATA_STR):null);
                 list.add(m);
             }
         } catch (JSONException e) {
